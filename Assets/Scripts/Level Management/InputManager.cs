d1ed004718d273e7bb373b888a0053941cf08bec
ScarletPhoenix5053 <45509046+ScarletPhoenix5053@users.noreply.github.com>
2019-02-28 14:15:15 +1300

    input system start

--- Assets/Scripts/Level Management/InputManager.cs
+++ Assets/Scripts/Level Management/InputManager.cs
@@ -6,17 +6,20 @@ namespace Sierra.AGPW.TenSecondAscencion
 {
     public class InputManager : MonoBehaviour
     {
-        public PlayerController Player;
+        public PlayerController PlayerOne;
+        public PlayerController PlayerTwo;
+
+        private ControllerInputGroup ControllerOneInput = new ControllerInputGroup(KeyCode3)
 
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                Player.TriggerInputJump();
+                PlayerOne.TriggerInputJump();
             }
             if (Input.GetKey(KeyCode.Space))
             {
-                Player.TriggerInputJumpHeld();
+                PlayerOne.TriggerInputJumpHeld();
             }
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
             {
@@ -24,8 +27,52 @@ namespace Sierra.AGPW.TenSecondAscencion
                 if (Input.GetKey(KeyCode.A)) input -= 1;
                 if (Input.GetKey(KeyCode.D)) input += 1;
 
-                Player.TriggerInputHorizontal(input);
+                PlayerOne.TriggerInputHorizontal(input);
             }
+        }
+    }
+    public interface IInputGroup
+    {
+        bool GetJump();
+        bool GetJumpHeld();
+        bool GetThrow();
+
+        float GetMotionHorizontal();
+        float GetMotionVertical();
+
+        Vector2 GetAimDir();
+    }
+    public struct KeyboardInputGroup
+    {
+
+    }
+    public struct ControllerInputGroup : IInputGroup
+    {
+        public ControllerInputGroup(KeyCode keyJump, KeyCode keyThrow)
+        {
+            _keyThrow = keyJump;
+            _keyJump = keyThrow;
+        }
+
+        private KeyCode _keyThrow;
+        private KeyCode _keyJump;
+
+        public bool GetJump() => Input.GetKeyDown(_keyJump);
+        public bool GetJumpHeld() => Input.GetKey(_keyJump);
+        public bool GetThrow() => Input.GetKey(_keyThrow);
+
+        public float GetMotionHorizontal()
+        {
+            return Input.GetAxisRaw("Horizontal");
+        }
+        public float GetMotionVertical()
+        {
+            return Input.GetAxisRaw("Vertical");
+        }
+
+        public Vector2 GetAimDir()
+        {
+            return new Vector2(Input.GetAxis("AimHor"), Input.GetAxis("AimVert")); 
         }
     }
 }
