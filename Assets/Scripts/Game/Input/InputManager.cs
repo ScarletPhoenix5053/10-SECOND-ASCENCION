8ce67586bff4b9243433b63161f25cdd4298539d
ScarletPhoenix5053 <45509046+ScarletPhoenix5053@users.noreply.github.com>
2019-03-03 16:26:16 +1300

    keyboard input setup

--- Assets/Scripts/Game/Input/InputManager.cs
+++ Assets/Scripts/Game/Input/InputManager.cs
@@ -9,6 +9,11 @@ namespace Sierra.AGPW.TenSecondAscencion
         {
             ControllerInput1 = new ControllerInputGroup(Player1ControllerIndex);
             ControllerInput2 = new ControllerInputGroup(Player2ControllerIndex);
+
+            var keyboardInputAxis1 = new KeyboardInputAxis(Player1Right, Player1Left);
+            var keyboardInputAxis2 = new KeyboardInputAxis(Player2Right, Player2Left);
+            KeyboardInput1 = new KeyboardInputGroup(keyboardInputAxis1, Player1Jump, Player1Throw);
+            KeyboardInput2 = new KeyboardInputGroup(keyboardInputAxis2, Player2Jump, Player2Throw);
         }
         private void Start()
         {
@@ -19,17 +24,30 @@ namespace Sierra.AGPW.TenSecondAscencion
             CheckControlsFor(Player1, ControllerInput1);
             CheckControlsFor(Player2, ControllerInput2);
 
+            CheckControlsFor(Player1, KeyboardInput1);
+            CheckControlsFor(Player2, KeyboardInput2);
+
             //LogAllKeyPresses()
         }
 
         public PlayerController Player1;
         public int Player1ControllerIndex = 1;
+        public KeyCode Player1Left = KeyCode.A;
+        public KeyCode Player1Right = KeyCode.D;
+        public KeyCode Player1Jump = KeyCode.W;
+        public KeyCode Player1Throw = KeyCode.F;
 
         public PlayerController Player2;
         public int Player2ControllerIndex = 2;
+        public KeyCode Player2Left = KeyCode.LeftArrow;
+        public KeyCode Player2Right = KeyCode.RightArrow;
+        public KeyCode Player2Jump = KeyCode.UpArrow;
+        public KeyCode Player2Throw = KeyCode.RightShift;
 
         private ControllerInputGroup ControllerInput1;
         private ControllerInputGroup ControllerInput2;
+        private KeyboardInputGroup KeyboardInput1;
+        private KeyboardInputGroup KeyboardInput2;
 
         private void LogControllerCountAndNames()
         {
@@ -80,13 +98,48 @@ namespace Sierra.AGPW.TenSecondAscencion
         bool GetThrow();
 
         float GetMotionHorizontal();
-        float GetMotionVertical();
-
-        Vector2 GetAimDir();
     }
-    public struct KeyboardInputGroup
+    public struct KeyboardInputGroup : IInputGroup
     {
+        public KeyboardInputGroup(KeyboardInputAxis horizontal, KeyCode keyJump, KeyCode keyThrow)
+        {
+            _keyThrow = keyThrow;
+            _keyJump = keyJump;
+            _axisHorizontalKeys = horizontal;
+        }
 
+        private readonly KeyboardInputAxis _axisHorizontalKeys;
+        private readonly KeyCode _keyJump;
+        private readonly KeyCode _keyThrow;
+
+        public bool GetJump() => Input.GetKeyDown(_keyJump);
+        public bool GetJumpHeld() => Input.GetKey(_keyJump);
+        public bool GetThrow() => Input.GetKeyDown(_keyThrow);
+
+        public float GetMotionHorizontal()
+        {
+            var motion = _axisHorizontalKeys.GetValue();
+            return motion;
+        }
+    }
+    public struct KeyboardInputAxis
+    {
+        public KeyboardInputAxis(KeyCode positive, KeyCode negative)
+        {
+            _positive = positive;
+            _negative = negative;
+        }
+
+        private readonly KeyCode _positive;
+        private readonly KeyCode _negative;
+
+        public float GetValue()
+        {
+            var value = 0f;
+            if (Input.GetKey(_positive)) value += 1;
+            if (Input.GetKey(_negative)) value -= 1;
+            return value;
+        }
     }
     public struct ControllerInputGroup : IInputGroup
     {
